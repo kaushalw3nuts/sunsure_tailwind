f9f488e7da8794611b1610f98de65ebc85941d61
kaushalw3nuts <kaushal.w3nuts@gmail.com>
2024-09-16 17:13:22 +0530

    meta description

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta discription="Create a Design" />
         <title>Homepage</title>
         <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
       </Head>
