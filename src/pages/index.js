ac18bb22d7f94b2342bdaec9fb7a95c6b397c131
kaushalw3nuts <kaushal.w3nuts@gmail.com>
2024-09-16 17:04:57 +0530

    error solved

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <title>Homepage</title>
         <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
       </Head>
