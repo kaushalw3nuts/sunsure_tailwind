4648cf344e8d8d7c532d32fa826524c36865886e
kaushalw3nuts <kaushal.w3nuts@gmail.com>
2024-09-23 16:28:42 +0530

    usestate hook practice

--- src/components/StateDemo.jsx
+++ src/components/StateDemo.jsx
@@ -3,20 +3,61 @@ import { useState } from "react";
 const StateDemo = () => {
 
 	const [demo, setDemo] = useState(false);
-
 	const demoWrap = () => {
 		setDemo(!demo);
+	}
+
+	const [value, setValue] = useState("");
+	const valueChange = (e) => {
+		setValue(e.target.value);
+	}
+
+	const [data, setData] = useState([]);
+	const allDataList = () => {
+		setData([...data, value]);
+	}
+
+	const [children, setChildren] = useState(['madhav', 'krish', 'arjun', 'karan', 'shikhar']);
+	const deleteData = (index) => {
+		const dubArray = [...children];
+		dubArray.splice(index, 1);
+		setChildren(dubArray);
 	}
 
 	return (
 		<>
 			<div className="container pb-10">
-				<h1 className="text-h1 font-medium text-gray_txt_01 flex items-center justify-center text-center flex-wrap" style={{color: demo ? 'gray' : 'blue'}}>
-					Making power move.
-				</h1>
-				{demo && <p className="text-white_txt font-normal text-p text-center mt-6">Lorem ipsum dolor</p>}
-				<div className="flex justify-center">
-					<button className="flex items-center justify-center mt-6 text-h5 bg-orange_bg py-4 px-10 rounded-lg" onClick={demoWrap}>{demo ? 'hide' : 'show'}</button>
+				<div>
+					<h1 className="text-h1 font-medium text-gray_txt_01 flex items-center justify-center text-center flex-wrap" style={{color: demo ? 'gray' : 'blue'}}>
+						Making power move.
+					</h1>
+					{demo && <p className="text-white_txt font-normal text-p text-center mt-6">Lorem ipsum dolor</p>}
+					<div className="flex justify-center">
+						<button className="flex items-center justify-center mt-6 text-h5 bg-orange_bg py-4 px-10 rounded-lg" onClick={demoWrap}>{demo ? 'hide' : 'show'}</button>
+					</div>
+				</div>
+				<div className="mt-10 text-center">
+					<input type="text" onChange={(e) => valueChange(e)} placeholder="enter name" style={{border: '1px solid #959595', background: 'transparent', padding: 12, borderRadius: 6, width: 240}} />
+					<button onClick={allDataList} style={{border: 'none', borderRadius: 6, background: '#959595', padding: 13, fontWeight: 'bold', marginLeft: 6, width: 100}}>Add</button>
+					<ul className="mt-5 text-start">
+						{data.map((item, index) => {
+							return (
+								<li key={index}>{item}</li>
+							)
+						})}
+					</ul>
+				</div>
+				<div className="mt-10">
+					<ul className="text-center">
+						{children.map((listing, index) => {
+							return (
+								<li key={index} className="text-p mt-3">
+									{listing}
+									<button onClick={() => deleteData(index)} style={{border: 'none', borderRadius: 6, background: '#959595', padding: 13, fontWeight: 'bold',fontSize: 14, marginLeft: 6, width: 100}}>Delete</button>
+								</li>
+							)
+						})}
+					</ul>
 				</div>
 			</div>
 		</>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,22 @@
 import '@/styles/fonts.css';
 import "@/styles/globals.css";
+import { ReactLenis, useLenis } from 'lenis/react'
 import Header from "@/components/layout/Header";
 
 export default function App({ Component, pageProps }) {
+
+  const lenis = useLenis(({ scroll }) => {
+		// called every scroll
+	})
+
   return (
     <>
-      <main className="main_wrap">
-        <Header />
-        <Component {...pageProps} />
-      </main>
+      <ReactLenis root>
+        <main className="main_wrap">
+          <Header />
+          <Component {...pageProps} />
+        </main>
+      </ReactLenis>
     </>
   );
 }
