d02228d556cf94b6152f5a86ad2424eb7fc6dc8d
kaushalw3nuts <kaushal.w3nuts@gmail.com>
2024-10-08 11:17:16 +0530

    form validation

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -11,7 +11,31 @@ const FormSection = () => {
 		contact: '',
 	};
 
+ 	const errorMsg = {
+		firstname: null,
+		lastname: null,
+		email: null,
+		password: null,
+		username: null,
+		contact: null,
+ 	}
+
 	const [formVal, setFormVal] = useState(modal);
+	const [formError, setFormError] = useState(errorMsg);
+
+	const validation = (key, value) => {
+		if(!value.length) {
+			setFormError({
+				...formError,
+				[key]: `${key} field is required`,
+			})
+		}else {
+			setFormError({
+				...formError,
+				[key]: null,
+			})
+		}
+	}
 
 	const getFormValue = (e) => {
 		const input = e.target
@@ -21,10 +45,17 @@ const FormSection = () => {
 			...formVal,
 			[key]: value
 		})
+
+		validation(key, value);
 	}
 
 	const signup = (e) => {
 		e.preventDefault();
+		const formSub = Object.keys(formVal);
+		for(let key of formSub) {
+			let value = formVal[key];
+			validation(key, value);
+		}
 	}
 
   	return (
@@ -37,32 +68,32 @@ const FormSection = () => {
 							<div className="flex flex-col gap-2">
 								<label className="text-p font-medium">Firstname</label>
 								<input onChange={getFormValue} type="text" name="firstname" placeholder="first name" className="bg-transparent border border-solid border-slate-500 px-4 py-3 rounded text-[16px] font-normal capitalize text-neutral-400" />
-								<span className="text-red-600 font-bold mt-1">Please fill out this field.</span>
+								{ formError.firstname && <span className="text-red-600 font-bold mt-1">{formError.firstname}</span> }
 							</div>
 							<div className="flex flex-col gap-2">
 								<label className="text-p font-medium">Lastname</label>
 								<input onChange={getFormValue} type="text" name="lastname" placeholder="last name" className="bg-transparent border border-solid border-slate-500 px-4 py-3 rounded text-[16px] font-normal capitalize text-neutral-400" />
-								<span className="text-red-600 font-bold mt-1">Please fill out this field.</span>
+								{ formError.lastname && <span className="text-red-600 font-bold mt-1">{formError.lastname}</span> }
 							</div>
 							<div className="flex flex-col gap-2">
 								<label className="text-p font-medium">Email</label>
 								<input onChange={getFormValue} type="email" name="email" placeholder="xyz@gmail.com" className="bg-transparent border border-solid border-slate-500 px-4 py-3 rounded text-[16px] font-normal lowercase text-neutral-400" />
-								<span className="text-red-600 font-bold mt-1">Please fill out this field.</span>
+								{ formError.email && <span className="text-red-600 font-bold mt-1">{formError.email}</span> }
 							</div>
 							<div className="flex flex-col gap-2">
 								<label className="text-p font-medium">Password</label>
 								<input onChange={getFormValue} type="password" name="password" placeholder="*******" className="bg-transparent border border-solid border-slate-500 px-4 py-3 rounded text-[16px] font-normal text-neutral-400" />
-								<span className="text-red-600 font-bold mt-1">Please fill out this field.</span>
+								{ formError.password && <span className="text-red-600 font-bold mt-1">{formError.password}</span> }
 							</div>
 							<div className="flex flex-col gap-2">
 								<label className="text-p font-medium">Username</label>
 								<input onChange={getFormValue} type="text" name="username" placeholder="@Username" className="bg-transparent border border-solid border-slate-500 px-4 py-3 rounded text-[16px] font-normal text-neutral-400" />
-								<span className="text-red-600 font-bold mt-1">Please fill out this field.</span>
+								{ formError.username && <span className="text-red-600 font-bold mt-1">{formError.username}</span> }
 							</div>
 							<div className="flex flex-col gap-2">
 								<label className="text-p font-medium">Contact no.</label>
 								<input onChange={getFormValue} type="number" name="contact" placeholder="9462318971" className="bg-transparent border border-solid border-slate-500 px-4 py-3 rounded text-[16px] font-normal text-neutral-400" />
-								<span className="text-red-600 font-bold mt-1">Please fill out this field.</span>
+								{ formError.contact && <span className="text-red-600 font-bold mt-1">{formError.contact}</span> }
 							</div>
 
 							<button className="bg-gradient-to-r from-teal-400 to-blue-500 rounded px-4 py-2 mt-4 text-lg font-medium">Submit</button>
